f8836cbd1f78ffce53c10792bb61f5f28074844e
Mark Bennett <mark@burmis.ca>
2023-01-03 20:49:43 -0700

    Get data on page load

--- src/routes/location/[locationId]/index.tsx
+++ src/routes/location/[locationId]/index.tsx
@@ -1,8 +1,51 @@
-import { component$ } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
+import { component$, Resource } from "@builder.io/qwik";
+import { RequestHandler, useEndpoint } from "@builder.io/qwik-city";
+
+interface CityDetails {
+  id: string;
+  name: string;
+  motto: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  createdAt: number;
+}
+
+export const onGet: RequestHandler = ({ params }) => {
+  const cityDetails: CityDetails = {
+    id: params.locationId,
+    name: "Edmonton",
+    motto: "Yeg",
+    location: {
+      lat: 53.5444,
+      lng: -113.4909,
+    },
+    createdAt: Date.now(),
+  };
+  return cityDetails;
+};
 
 export default component$(() => {
-  const location = useLocation();
-  const locationId = location.params.locationId;
-  return <div>Test location = {locationId}</div>;
+  const locationData = useEndpoint<CityDetails>();
+  return (
+    <Resource
+      value={locationData}
+      onPending={() => <div>Loading...</div>}
+      onRejected={() => <div>ERROR!</div>}
+      onResolved={(location) => {
+        return (
+          <>
+            <h1>
+              {location.name} ({location.id})
+            </h1>
+            <p>{location.motto}</p>
+            <p>Lat: {location.location.lat}</p>
+            <p>Lng: {location.location.lng}</p>
+            <p>Created At: {location.createdAt}</p>
+          </>
+        );
+      }}
+    />
+  );
 });
